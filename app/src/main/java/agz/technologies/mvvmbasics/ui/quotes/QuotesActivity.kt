21713409d5461cfb64d186c9f7b9d6d09ecdfd8b
andre <andresro0198@gmail.com>
2021-06-11 14:32:25 +0200

    Project setup

---
+++ app/src/main/java/agz/technologies/mvvmbasics/ui/quotes/QuotesActivity.kt
@@ -0,0 +1,12 @@
+package agz.technologies.mvvmbasics.ui.quotes
+
+import agz.technologies.mvvmbasics.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
---
+++ app/src/main/java/agz/technologies/mvvmbasics/ui/quotes/QuotesViewModel.kt
@@ -0,0 +1,6 @@
+package agz.technologies.mvvmbasics.ui.quotes
+
+import agz.technologies.mvvmbasics.data.QuoteRepository
+
+class QuotesViewModel (private val quoteRepository: QuoteRepository) {
+}
